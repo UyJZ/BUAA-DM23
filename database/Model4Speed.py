28a93db5594eb8b59a3ffaaad2b22237407bdbce
JingYuChou <zjy262702057@outlook.com>
2023-12-24 12:15:48 +0800

    for server

--- database/Model4Speed.py
+++ database/Model4Speed.py
@@ -6,6 +6,8 @@ import numpy as np
 from shapely.geometry import Point, LineString
 from concurrent.futures import ThreadPoolExecutor
 import concurrent.futures
+from tqdm import tqdm
+from rtree import index as rtree_index
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 graphPath = os.path.join(script_dir, 'data', 'road_graph.gpickle')
@@ -45,6 +47,39 @@ def getRoadID(from_x, from_y, to_x, to_y):
     key = (from_x, from_y, to_x, to_y)
     return coordinate_to_road_id.get(key, -1)
 
+def find_closest_edge_1(graph, coordinates):
+    # 创建空间索引
+    idx = rtree_index.Index()
+    for i, (edge_start, edge_end, data) in enumerate(graph.edges(data=True)):
+        start_coord, end_coord = np.array(edge_start), np.array(edge_end)
+        idx.insert(i, (min(start_coord[0], end_coord[0]), min(start_coord[1], end_coord[1],
+                                                               max(start_coord[0], end_coord[0]),
+                                                               max(start_coord[1], end_coord[1]))))
+    
+    # 处理每个点
+    results = []
+    for coordinate in coordinates:
+        point = Point(coordinate)
+        min_distance = float('inf')
+        closest_edge = None
+        
+        # 查询空间索引以获取潜在的边
+        for i in idx.intersection((coordinate[0], coordinate[1], coordinate[0], coordinate[1])):
+            edge_start, edge_end, data = graph.edges(data=True)[i]
+            start_coord, end_coord = np.array(edge_start), np.array(edge_end)
+            distance = LineString([start_coord, end_coord]).distance(point)
+            
+            if distance < min_distance:
+                min_distance = distance
+                closest_edge = (edge_start, edge_end)
+        
+        if closest_edge is not None:
+            results.append(getRoadID(*closest_edge[0], *closest_edge[1]))
+        else:
+            results.append(-1)
+    
+    return results
+
 def find_closest_edge(graph, coordinate):
     point = Point(coordinate)
     closest_edge = None
@@ -71,19 +106,21 @@ def find_closest_edge(graph, coordinate):
 def find_edge_for_trajectory_parallel(graph, trajectory):
     results = []
 
-    with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(find_closest_edge, graph, ast.literal_eval(row['coordinates'])) for _, row in trajectory.iterrows()]
-        
-        for future in concurrent.futures.as_completed(futures):
-            road_id = future.result()
-            results.append({'road_id': road_id})
-            print('Finished processing road_id: {}'.format(road_id))
+    with tqdm(total=len(trajectory)) as pbar:  # 使用 tqdm 创建进度条
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(find_closest_edge, graph, ast.literal_eval(row['coordinates'])) for _, row in trajectory.iterrows()]
+            
+            for future in concurrent.futures.as_completed(futures):
+                road_id = future.result()
+                results.append({'road_id': road_id})
+                pbar.update(1)  # 更新进度条
+                print('Finished processing road_id: {}'.format(road_id))
     
     result_df = pd.DataFrame(results)
     result_df['coordinate'] = trajectory['coordinates'].values
     return result_df
 
-# Add roadId to trajectory and save to a new CSV file
+# 将 roadId 添加到轨迹并保存到新的 CSV 文件
 result_df_parallel = find_edge_for_trajectory_parallel(graph, traj)
 traj['roadId'] = result_df_parallel['road_id'].values
 traj.to_csv(os.path.join(script_dir, 'data', 'traj_roadId.csv'), index=False)
